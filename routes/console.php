6fdbffbf9aaf1500ccbb1fc27e389937184f333e
wisnu baldas <wisnubaldas@gmail.com>
2024-05-06 13:19:25 +0700

    lanjut report

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-
+// use Illuminate\Support\Facades\Schedule;
 /*
 |--------------------------------------------------------------------------
 | Console Routes
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Schedule::command('some-service:sync')->daily();
---
+++ scheduler.bat
@@ -0,0 +1,6 @@
+:loop
+
+cd C:\laragon\www\tps-online-dbm\
+php artisan schedule:run >> my_log.txt
+
+goto loop
